9008e22d09223214d9f572767edb8b22d58bdfe5
David Godfrey <david.godfrey99@gmail.com>
2022-05-01 17:10:11 -0400

    Time: 0 ms (100.00%), Space: 6.5 MB (52.29%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,19 +1,12 @@
 class Solution {
-public:
-    int lengthOfLastWord(string s) {
-        reverse(s.begin(), s.end());
-        for (int i = 0; i < s.size(); i++) {
-            if (isalpha(s[i])) {
-                int j = i;
-                while (j <= s.size()) {
-                    if (j == s.size())
-                        return j - i;
-                    if (!isalpha(s[j]))
-                        return j - i;
-                    j++;
-                }
-            }
-        }
-        return 0;
-    }
+ public:
+  int lengthOfLastWord(string s) {
+    int n = s.size();
+    for (int i = n - 1; i >= 0; i--)
+      if (isalpha(s[i]))
+        for (int j = i; j >= -1; j--)
+          if (j == -1 || !isalpha(s[j]))
+            return i - j;
+    return 0;
+  }
 };
