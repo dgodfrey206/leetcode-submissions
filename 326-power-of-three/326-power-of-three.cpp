704733d5d21527d17c978d47e2d1849992e7b586
David Godfrey <david.godfrey99@gmail.com>
2022-05-06 21:46:11 -0400

    Time: 20 ms (75.80%), Space: 5.7 MB (99.02%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        while (n) {
+            if (n == 3)
+                return true;
+            if (n < 3)
+                return n == 1;
+            if (n % 3 != 0)
+                return false;
+            n /= 3;
+        }
+        return false;
+    }
+};
