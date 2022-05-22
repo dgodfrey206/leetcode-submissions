d50d1e0259e2171ec0fb93831d8890171e93259d
David Godfrey <david.godfrey99@gmail.com>
2022-05-22 13:30:14 -0400

    Time: 22 ms (26.42%), Space: 6.6 MB (56.98%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -4,14 +4,11 @@ public:
         if (n == 1)
             return "1";
         string sayLess = countAndSay(n-1);
-        int count = 1;
         string res;
-        for (int i = 1; i <= sayLess.size(); i++) {
+        for (int i = 1, count = 1; i <= sayLess.size(); i++, count++) {
             if (i == sayLess.size() || sayLess[i] != sayLess[i-1]) {
                 res += to_string(count) + sayLess[i-1];
-                count = 1;
-            } else {
-                count++;
+                count = 0;
             }
         }
         return res;
